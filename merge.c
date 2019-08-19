5948a94023071a584cacfd01bb4802efda9b0c57
Gabriel Nascimento <gabriel.amorim.nascimento@gmail.com>
2019-08-19 10:50:45 -0300

    Change merge sort to its final state Add new file to gitignore

--- merge.c
+++ merge.c
@@ -1,57 +1,95 @@
+#include <stdlib.h>
 #include <stdio.h>
 
-
-int merge(int *array, int m, int l, int r){
-	int nr = r - m;
-	int nl = m - l + 1
-
-	int i, j, k, right[nr], left[nl];
-	
-
-	for (i = 0; i < nl; ++i)
-	{
-		left[i] = array[l+i];
-	}
-
-	for (i = 0; i < nr; ++i)
-	{
-		right[i] = array[m+l+j];
-	}
-
-	i, j, k = 0, 0, l;
-
-	while(i < nl && j < nr){
-		if (left[i] <= R[j])
-		{
-			array[k] = left[i];
-			i++;
-		}
-		else{
-			array[k] = right[j];
-			j++;
-		}
-		k++
-	}
-
-	while(i < nl){
-		array[k] = left[i];
-		i++;
-		k++;
-	}
-
-	while(j < nr){
-		array[k] = right[i];
-		i++;
-		k++;
-	}
+void printa(int *array, int size){
+    for (int i=0; i < size; i++){
+        printf("%d ", array[i]);
+    }
+    printf("\n");
 }
 
-int merge_sort(){
+void merge(int *array, int l, int m, int r)
+{
+    int nr = r - m;
+    int nl = m - l + 1;
 
+    int i, j, k, right[nr], left[nl];
+
+    for (i = 0; i < nl; ++i)
+    {
+        left[i] = array[l+i];
+    }
+
+    for (j = 0; j < nr; ++j)
+    {
+        right[j] = array[m+1+j];
+    }
+
+    i = 0;
+    j = 0;
+    k = l;
+
+    while(i < nl && j < nr)
+    {
+        if (left[i] <= right[j])
+        {
+            array[k] = left[i];
+            i++;
+        }
+        else
+        {
+            array[k] = right[j];
+            j++;
+        }
+        k++;
+    }
+
+    while(i < nl)
+    {
+        array[k] = left[i];
+        i++;
+        k++;
+    }
+
+    while(j < nr)
+    {
+        array[k] = right[j];
+        j++;
+        k++;
+    }
+}
+
+void merge_sort(int *array, int left, int right)
+{
+    if (left < right)
+    {
+        int m = left+(right-left)/2;
+
+        merge_sort(array, left, m);
+        merge_sort(array, m+1, right);
+
+        merge(array, left, m, right);
+    }
 }
 
 int main(int argc, char const *argv[])
 {
-	
-	return 0;
+    int n, nums[1010];
+
+    scanf("%d", &n);
+
+    for (int i = 0; i < n; ++i)
+    {
+        scanf("%d", &nums[i]);
+    }
+
+    printa(nums, n);
+
+    merge_sort(nums, 0, n-1);
+
+    printa(nums, n);
+
+
+
+    return 0;
 }
