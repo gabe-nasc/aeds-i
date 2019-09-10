2fb7f55da54608453e8607177a121b2fe3a284f0
Gabriel Nascimento <gabriel.amorim.nascimento@gmail.com>
2019-09-10 12:06:48 -0300

    Add functions to insert and remove elements at the top of a struct named darray, which is an dynamically allocated array. Stack functionality is complete.

--- stacks_queues_deques.c
+++ stacks_queues_deques.c
@@ -1,32 +1,69 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void add_top(int **array, int n){
-    size_t size = sizeof(array)/sizeof(int);
-    *array = realloc(*array, (size+1)*sizeof(int));
-    puts("not yet");
-    array[size+1] = n;
-    puts("gallahad");
+typedef struct 
+{
+    int *array;
+    int size;
+    int used;
+} darray;
+
+void alloc_array(darray *a, int init_size){
+    a->array = (int *) malloc(init_size*sizeof(int));
+    a->size = init_size;
+    a->used = 0;
+}
+
+void add_top(darray *a, int elem){
+    if (a->used == a->size)
+    {
+        a->size += 1;
+        a->array = (int *) realloc(a->array, (a->size)*sizeof(int));
+    }
+    a->array[a->used] = elem;
+    a->used += 1;
+}
+
+void pop_top(darray *a){  
+    darray tmp;
+    alloc_array(&tmp, (a->used)-1);
+
+    for (int i = 0; i < (a->used)-1; i++)
+    {
+        add_top(&tmp, a->array[i]);
+    }
+    a->size = tmp.size;
+    a->used = tmp.used;
+    free(a->array);
+    a->array = tmp.array;    
 }
 
 int main() {
     int n;
 
-    // scanf("%d", &n);
+    darray lista;
+    scanf("%d", &n);
+    alloc_array(&lista, n);
 
-    int *lista = malloc(4*sizeof(int));
-
-    for (size_t i = 0; i < 4; i++)
+    for (size_t i = 0; i < n; i++)
     {   
-        lista[i] = i;
-        printf("%d", lista[i]);
+        add_top(&lista, i);
+        printf("%d", lista.array[i]);
     }
     puts("");
 
-    add_top(&lista, 5);
-    for (size_t i = 0; i < 5; i++)
+    add_top(&lista, n);
+    
+    for (size_t i = 0; i < lista.used; i++)
     {
-        printf("%d", lista[i]);
+        printf("%d", lista.array[i]);
+    }
+    pop_top(&lista);
+    pop_top(&lista);
+    pop_top(&lista);
+    for (size_t i = 0; i < lista.used; i++)
+    {
+        printf("%d", lista.array[i]);
     }
     puts("");
     return 0;
