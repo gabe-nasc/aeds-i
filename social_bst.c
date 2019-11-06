d35aecf1f27c3b1a25063e1358c3c2f0acfab63d
Gabriel Nascimento <gabriel.amorim.nascimento@gmail.com>
2019-11-06 15:36:19 -0300

    Add rudimentary user input interface Add info function to retrieve information from a user Add name variable to node type A

--- social_bst.c
+++ social_bst.c
@@ -1,8 +1,10 @@
-#include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
 
 struct nodeA {
     int data;
+    char nome[101];
     struct nodeA *pai;
     struct nodeA *menor;
     struct nodeA *maior;
@@ -16,13 +18,16 @@ struct nodeB {
     struct nodeB *maior;
 };
 
-struct nodeA* allocA(int data){
+struct nodeA* allocA(int data, char *nome){
     struct nodeA *new = (struct nodeA *) malloc(sizeof(struct nodeA));
+    
+    strcpy(new->nome, nome);
     new->data = data;
-    new->pai = NULL;
+
     new->maior = NULL;
     new->menor = NULL;
     new->amgs = NULL;
+    new->pai = NULL;
 
     return new;
 }
@@ -69,27 +74,27 @@ struct nodeB* findB(struct nodeB* head, int data)
     return findB(head->menor, data);
 }
 
-void insertA(struct nodeA *head, int data){
+void insertA(struct nodeA *head, int data, char *nome){
     if (head->data > data)
     {
         if (head->menor == NULL)
         {
-            head->menor = allocA(data);
+            head->menor = allocA(data, nome);
             head->menor->pai = head;
             return;
         }
 ;
-        insertA(head->menor, data);
+        insertA(head->menor, data, nome);
     }
     else
     {
         if (head->maior == NULL)
         {
-            head->maior = allocA(data);
+            head->maior = allocA(data, nome);
             head->maior->pai = head;
             return;
         }
-        insertA(head->maior, data);
+        insertA(head->maior, data, nome);
     }
 }
 
@@ -155,17 +160,95 @@ int consulta_amizade(struct nodeA *head, int a, int b){
     }    
 }
 
-void print_ordem(struct nodeA *head)
+void print_ordemA(struct nodeA *head)
 {
     if (head != NULL)
     {
-        print_ordem(head->menor);
-        printf("%d ", head->data);
-        print_ordem(head->maior);
+        print_ordemA(head->menor);
+        printf("%d %s\n", head->data, head->nome);
+        print_ordemA(head->maior);
     }
+}
+
+void print_ordemB(struct nodeB *head)
+{
+    if (head != NULL)
+    {
+        print_ordemB(head->menor);
+        printf("%d ", head->data);
+        print_ordemB(head->maior);
+    }
+}
+
+void info(struct nodeA *head, int data){
+    struct nodeA *ptr = find(head, data);
+    printf("%s\nID: %d\nAmizades: ", ptr->nome, ptr->data);
+    print_ordemB(ptr->amgs);
+    puts("");
 }
 
 int main(int argc, char const *argv[])
 {
+    struct nodeA *head;
+    int count = 1;
+    char cmd[120];
+
+    while (1)
+    {
+        printf(">> ");
+        scanf("%[^\n]%*c", cmd);
+        puts("");
+
+        if (!strcmp(cmd, "help"))
+        {
+            puts("help - mostra ajuda com comandos");
+            puts("new x - adiciona uma nova pessoa (x) na rede");
+            puts("nfd x y - adicionar uma nova amizade entre (x) e (y)");
+            puts("chk x y - checar se existe amizade entre (x) e (y)");
+            puts("prt - imprime todas as pessoas na rede");
+        }
+        else if (!strncmp(cmd, "new", 3))
+        {
+            if (head == NULL)
+            {
+                head = allocA(count, cmd+4);
+            }
+            else
+            {
+                insertA(head, count, cmd+4);
+            }
+            count += 1;
+        }
+        else if (!strncmp(cmd, "nfd", 3))
+        {
+            int p1, p2;
+            sscanf(cmd, "%*s %d %d", &p1, &p2);
+            insertB(head, p1, p2);
+        }
+        else if (!strncmp(cmd, "chk", 3))
+        {
+            int p1, p2;
+            sscanf(cmd, "%*s %d %d", &p1, &p2);
+            printf("%d\n", consulta_amizade(head, p1, p2));
+        }
+        else if (!strncmp(cmd, "prt", 3))
+        {
+            print_ordemA(head);
+            puts("");
+        }
+        else if (!strncmp(cmd, "bsc", 3))
+        {
+
+            int p1;
+            sscanf(cmd, "%*s %d", &p1);
+            info(head, p1);
+        }
+        else if (!strcmp(cmd, "quit"))
+        {
+            break;
+        }
+        
+    }
+
     return 0;
 }
